9838b004dceff30233c06f277947319b211f9dc9
Mahammad Prabej Hussain <contact.prabej@gmail.com>
2024-02-15 20:16:38 +0545

    Before Lunch

--- app.js
+++ app.js
@@ -39,36 +39,36 @@ app.use(passport.session());
 
 mongoose.connect('mongodb://localhost:27017/UserDB');
 
-const patientSchema  = mongoose.Schema({
+const patientSchema = mongoose.Schema({
     name: String,
     phone: String,
     age: String,
     doctor: String,
     hospital: String,
     receipt: String,
-    type:String
+    type: String
 });
 
-const Patinet = mongoose.model('patient',patientSchema);
+const Patinet = mongoose.model('patient', patientSchema);
 
 const doctorSchema = mongoose.Schema({
     name: String,
     type: String,
-    attend : String,
+    attend: String,
     img: String,
 });
 
-const Doctor = mongoose.model('doctor',doctorSchema);
+const Doctor = mongoose.model('doctor', doctorSchema);
 
 const userSchema = mongoose.Schema({
     username: String,
     password: String,
     type: String,
     doctors: [doctorSchema],
-    patients :[patientSchema],
+    patients: [patientSchema],
     img: String,
-    eSewa : String,
-    eSewaNo : String
+    eSewa: String,
+    eSewaNo: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -90,7 +90,7 @@ app.post('/register', async (req, res) => {
             console.log(err);
         } else {
             passport.authenticate('local')(req, res, async () => {
-                await User.updateOne({ _id: req.user._id }, { $set: { type: type,img:'img.png' } });
+                await User.updateOne({ _id: req.user._id }, { $set: { type: type, img: 'img.png' } });
                 res.redirect('/account');
             });
         }
@@ -118,10 +118,15 @@ app.post('/login', (req, res) => {
 });
 
 
-app.get('/account', async(req, res) => {
+app.get('/account', async (req, res) => {
     if (req.isAuthenticated()) {
-        const data = await User.findById(req.user._id);
-        
+        if (!req.user.hospital) {
+            const data = await User.findById(req.user.id);
+            console.log(data);
+        } else {
+            res.render('account', { data: req.user, doctors: req.user.doctors });
+        }
+
     } else {
         res.redirect('/login');
     }
@@ -129,17 +134,17 @@ app.get('/account', async(req, res) => {
 
 
 app.get('/account/doctors', async (req, res) => {
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         const doctors = await req.user.doctors;
-        res.render('AddDoctor',{data:doctors});
-    }else{
+        res.render('AddDoctor', { data: doctors });
+    } else {
         res.redirect('/login');
     }
-    
+
 });
 
-app.post('/addDocs',uploads.single('profile'),async(req,res)=>{
-    const newDoctor =  new Doctor({
+app.post('/addDocs', uploads.single('profile'), async (req, res) => {
+    const newDoctor = new Doctor({
         name: req.body.name,
         type: req.body.type,
         attend: 'Offline',
@@ -151,45 +156,45 @@ app.post('/addDocs',uploads.single('profile'),async(req,res)=>{
     res.redirect('/account/doctors');
 });
 
-app.get('/register',(req,res)=>{
+app.get('/register', (req, res) => {
     res.render('signup');
 });
 
-app.post('/updateTime',async(req,res)=>{
+app.post('/updateTime', async (req, res) => {
     const doctor = await Doctor.findById(req.body.on);
     let presence = doctor.attend;
-    if(presence==='Offline'){
+    if (presence === 'Offline') {
         doctor.attend = 'Online';
-    }else{
+    } else {
         doctor.attend = 'Offline';
     }
     await doctor.save();
-    await User.updateOne({_id:req.user._id,'doctors._id':req.body.on},
-    {$set:{'doctors.$.attend':doctor.attend}});
+    await User.updateOne({ _id: req.user._id, 'doctors._id': req.body.on },
+        { $set: { 'doctors.$.attend': doctor.attend } });
     res.redirect('/account/doctors');
 });
 
-app.get('/hospitals',async(req,res)=>{
-    const hospitals = await User.find({type:'hospital'});
-    res.render('hospitals',{data:hospitals});
+app.get('/hospitals', async (req, res) => {
+    const hospitals = await User.find({ type: 'hospital' });
+    res.render('hospitals', { data: hospitals });
 });
 
-app.get('/myaccount',async(req,res)=>{
-    if(req.isAuthenticated()){
-        res.render('myaccount',{pic:req.user.img});
-    }else{
+app.get('/myaccount', async (req, res) => {
+    if (req.isAuthenticated()) {
+        console.log(req.user);
+    } else {
         res.redirect('/login');
     }
 });
 
-app.post('/updatePofile',uploads.single('profile'),async(req,res)=>{
+app.post('/updatePofile', uploads.single('profile'), async (req, res) => {
     let profilePic = req.user.img;
-    const data = await User.updateOne({_id:req.user._id},{$set:{img:req.file.filename}});
-    if(profilePic!=='img.png'){
-        fs.unlink('./public/uploads/' + profilePic,(err)=>{
-            if(err){
+    const data = await User.updateOne({ _id: req.user._id }, { $set: { img: req.file.filename } });
+    if (profilePic !== 'img.png') {
+        fs.unlink('./public/uploads/' + profilePic, (err) => {
+            if (err) {
                 console.log(err);
-            }else{
+            } else {
                 console.log('File Deleted Successfully!');
             }
         });
@@ -197,156 +202,162 @@ app.post('/updatePofile',uploads.single('profile'),async(req,res)=>{
     res.redirect('/myaccount');
 });
 
-app.post('/updatePay',async(req,res)=>{
-    const data = await User.updateOne({_id:req.user._id},{$set:{eSewa:req.body.eSewa,eSewaNo: req.body.eSewaNo}});
+app.post('/updatePay', async (req, res) => {
+    const data = await User.updateOne({ _id: req.user._id }, { $set: { eSewa: req.body.eSewa, eSewaNo: req.body.eSewaNo } });
     console.log(data);
     res.redirect('/myaccount');
 });
 let hospitalName;
 let doctorName;
 
-app.get('/doctors',async(req,res)=>{
-    const doctors = await User.find({type:'doctor'});
-    res.render('iDoctors',{data:doctors});
+app.get('/doctors', async (req, res) => {
+    const doctors = await User.find({ type: 'doctor' });
+    res.render('iDoctors', { data: doctors });
 });
 
-app.post('/doctors',(req,res)=>{
+app.post('/doctors', (req, res) => {
     hospitalName = req.body.doctor;
-    doctorName ='1';
+    console.log(hospitalName);
+    doctorName = '1';
     res.redirect('/billing');
 });
 
-app.get('/hospitals/:text',async(req,res)=>{
+app.get('/hospitals/:text', async (req, res) => {
     hospitalName = req.params.text;
     const hospital = await User.findByUsername(req.params.text);
-    res.render('doctors',{data:hospital.doctors});
+    res.render('doctors', { data: hospital.doctors });
 });
 
-app.post('/preBilling',(req,res)=>{
+app.post('/preBilling', (req, res) => {
     doctorName = req.body.doctor;
     res.redirect('/billing');
 });
 
-app.get('/billing',async(req,res)=>{
+app.get('/billing', async (req, res) => {
     let isAuth;
     const data = await User.findByUsername(hospitalName);
     let eSewaUrl = `"eSewa_id:":"${data.eSewaNo}","name":"${data.eSewa}"`;
-    if(req.isAuthenticated() &&req.user.type==='patient'){
+    if (req.isAuthenticated() && req.user.type === 'patient') {
         isAuth = 1;
-    }else{
+    } else {
         isAuth = 0;
     }
-    qrCode.toDataURL(eSewaUrl,(err,url)=>{
-        res.render('billing',{qr:url,auth:isAuth});
+    qrCode.toDataURL(eSewaUrl, (err, url) => {
+        res.render('billing', { qr: url, auth: isAuth });
     });
 });
 
 let patId;
 
-app.post('/billing',uploads.single('receipt'),async(req,res)=>{
-    const {name,phone,age} = req.body;
-    if(req.body.type==='Offline'){
-        const newPatients = new Patinet({
-        name: req.body.name,
-        phone: req.body.phone,
-        age:req.body.age,
-        receipt: req.file.filename,
-        doctor: doctorName,
-        hospital: hospitalName,
-        type: 'Offline'
-    });
-    const saved = await newPatients.save();
-    patId=saved._id;
-    const hospital = await User.findByUsername(hospitalName);
-    hospital.patients.push(saved);
-    hospital.save();
-    }else{
+app.post('/billing', uploads.single('receipt'), async (req, res) => {
+    const { name, phone, age } = req.body;
+    if (req.body.type === 'Offline') {
         const newPatients = new Patinet({
             name: req.body.name,
             phone: req.body.phone,
-            age:req.body.age,
+            age: req.body.age,
+            receipt: req.file.filename,
+            doctor: doctorName,
+            hospital: hospitalName,
+            type: 'Offline'
+        });
+        const saved = await newPatients.save();
+        patId = saved._id;
+        const hospital = await User.findByUsername(hospitalName);
+        hospital.patients.push(saved);
+        hospital.save();
+    } else {
+        const newPatients = new Patinet({
+            name: req.body.name,
+            phone: req.body.phone,
+            age: req.body.age,
             receipt: req.file.filename,
             doctor: doctorName,
             hospital: hospitalName,
             type: 'Online'
         });
         const saved = await newPatients.save();
-        patId=saved._id;
+        patId = saved._id;
         const hospital = await User.findByUsername(hospitalName);
         hospital.patients.push(saved);
         hospital.save();
     }
-    
-    if(req.body.type==='Online'){
+
+    if (req.body.type === 'Online') {
         let data;
-        if(doctorName==='1'){
+        if (doctorName === '1') {
+            console.log(hospitalName);
             data = await User.findByUsername(hospitalName);
-            console.log(data);
-        }else{
-            data = await Doctor.find({name:doctorName});
+            let doctor = data;
+            req.user.doctors.push(doctor);
+            await req.user.save();
+            res.redirect('/account');
+        } else {
+            data = await Doctor.find({ name: doctorName });
+            let doctor = data[0];
+            req.user.doctors.push(doctor);
+            await req.user.save();
+            res.redirect('/account');
         }
-        
-        let doctor = data[0];
-        req.user.doctors.push(doctor);
-        await req.user.save();
-        res.redirect('/account');
-    }else{
+
+
+    } else {
         res.redirect('/qr');
     }
-    
+
 });
 
-app.get('/qr',(req,res)=>{
-    qrCode.toDataURL('localhost:3000/patients/'+patId,(err,url)=>{
-        res.render('qr',{qr:url});
+app.get('/qr', (req, res) => {
+    qrCode.toDataURL('localhost:3000/patients/' + patId, (err, url) => {
+        res.render('qr', { qr: url });
     });
 });
 
-app.get('/patients/:text',async(req,res)=>{
+app.get('/patients/:text', async (req, res) => {
     const patient = await Patinet.findById(req.params.text);
-    res.render('patDisplay',{data:patient});
+    res.render('patDisplay', { data: patient });
 });
 
-app.get('/account/patients',async(req,res)=>{
-    if(req.isAuthenticated() && req.user.type!=='patients'){
+app.get('/account/patients', async (req, res) => {
+    if (req.isAuthenticated() && req.user.type !== 'patients') {
         const hospital = await User.findById(req.user._id);
-        res.render('patients',{data:hospital.patients,username:req.user.username});;
-    }else{
+        res.render('patients', { data: hospital.patients, username: req.user.username });;
+    } else {
         res.redirect('/login');
     }
 });
 let newUser;
-app.post('/chat',(req,res)=>{
+app.post('/chat', (req, res) => {
     newUser = req.user.username;
     res.redirect('/chat');
 });
-let users={};
-io.on('connection',socket=>{
-    socket.on('user-joined',(name)=>{
+let users = {};
+io.on('connection', socket => {
+    socket.on('user-joined', (name) => {
         users[socket.id] = name;
-        socket.broadcast.emit('new-user',`${name} joined the chat !`);
+        socket.broadcast.emit('new-user', `${name} joined the chat !`);
     });
 
-    socket.on('receive',(data)=>{
-        socket.broadcast.emit('send',{userName: data.user, msg: data.msg});
+    socket.on('receive', (data) => {
+        socket.broadcast.emit('send', { userName: data.user, msg: data.msg });
     });
 
-    socket.on('disconnect',(data)=>{
-        socket.broadcast.emit('leave',{userName:users[socket.id],msg:'left the chat !'});
+    socket.on('disconnect', (data) => {
+        socket.broadcast.emit('leave', { userName: users[socket.id], msg: 'left the chat !' });
     });
-    
+
 });
 
-app.get('/chat',(req,res)=>{
-    res.render('chat',{name:newUser});
+app.get('/chat', (req, res) => {
+    res.render('chat', { name: newUser });
 });
 
-app.get('/logout', function(req, res, next){
-    req.logout(function(err) {
-      if (err) { return next(err); }
-      res.redirect('/login');
+app.get('/logout', function (req, res, next) {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        res.redirect('/login');
     });
-  });
+});
 
 server.listen(3000, () => {
     console.log('Server is running at port 3000.');
